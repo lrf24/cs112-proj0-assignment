b5b5cffdfe6c0cbcac6cf2ac1433353aa2a849d6
Landon Faber <lrf24@calvin.edu>
2023-01-14 14:55:47 -0500

    Finished Project 0

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
+     
+    float arr[20];
+    float *point;
+    point = &arr[7];
+    *point = 17.771;
+    cout << arr[7] << endl << point << endl << *point << endl;
 
     return 0;
 }
